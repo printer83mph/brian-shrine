e440fb567ec4397a685452c5a83c4df5a9b8c31b
Thomas Shaw <printer.83mph@gmail.com>
2019-01-31 10:39:46 -0500

    Initial files

---
+++ public/javascripts/chatroom.js
@@ -0,0 +1,48 @@
+(function () {
+
+    var chatLog, message, sendButton;
+    var socket = io();
+
+    function createMessage(user, message) {
+        var out = document.createElement("li");
+        if (user) {
+            let name = document.createElement("h2");
+            name.innerHTML = user;
+            out.appendChild(name);
+        }
+        let msg = document.createElement("p");
+        msg.innerHTML = message;
+        out.appendChild(msg);
+        return out;
+    }
+
+    window.onload = function () {
+        chatLog = document.getElementById("chatlog");
+        message = document.getElementById("message");
+        sendButton = document.getElementById("send");
+
+        let name = prompt("Enter your name") || "anon";
+
+        socket.emit("name", name);
+
+        socket.on('name', function(name) {
+            document.getElementById("name").innerHTML = name;
+        });
+
+        sendButton.onclick = function (e) {
+            e.preventDefault;
+            socket.emit("chat message", message.value);
+            message.value = "";
+            return false;
+        };
+
+        socket.on('chat message', function (user, msg) {
+            chatLog.appendChild(createMessage(user, msg));
+            chatLog.scrollTop = chatLog.scrollHeight;
+        });
+
+
+
+    }
+
+})();
---
+++ routes/index.js
@@ -0,0 +1,54 @@
+var express = require('express');
+var socket_io = require('socket.io');
+var router = express.Router();
+
+var io = socket_io();
+
+var users = {};
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Brian Shrine'
+  });
+});
+
+/* GET game page */
+router.get('/game', function (req, res, next) {
+  res.render('game', {
+    title: 'Brian Game'
+  });
+});
+
+/* GET game page */
+router.get('/chat', function (req, res, next) {
+  res.render('chat', {
+    title: 'Brian Chat'
+  });
+});
+
+io.on('connection', function (socket) {
+  users[socket.id] = "anon";
+  socket.on('name', function (nm) {
+    let nam = nm;
+    for (var scId in users) {
+      if (users[scId] == nam) {
+        nam += "_fake";
+      }
+    }
+    users[socket.id] = nam;
+    socket.emit('name', users[socket.id]);
+    io.emit('chat message', null, `${nam} has joined Brian Chat`);
+  });
+  socket.on('chat message', function (msg) {
+    io.emit('chat message', users[socket.id], msg);
+  });
+  socket.on('disconnect', function() {
+    io.emit('chat message', null, `${users[socket.id]} has disconnected`);
+    users[socket.id] = null;
+  });
+});
+
+router.io = io
+
+module.exports = router;
